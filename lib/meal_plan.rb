50193c82bce4de7bb4f0cdb48d69bdb8ac7850d6
YUKIKO IKEDA <applelele@gmail.com>
2020-04-23 23:38:33 +0000

    added some definitions to cli.rb

--- lib/meal_plan.rb
+++ lib/meal_plan.rb
@@ -1,5 +1,8 @@
+# enviroment file
 require_relative "./meal_plan/version"
 require_relative "./meal_plan/cli"
+
+require "pry"
 
 module MealPlan
   class Error < StandardError; end
--- lib/meal_plan/cli.rb
+++ lib/meal_plan/cli.rb
@@ -2,5 +2,20 @@ class MealPlan::CLI
 
   def call
     puts "Welcome to Meal Plan"
+    # binding.pry
+    # get_exist_keywords
+    # get_user_keyword
+    # get_recipes
+    # list_recipes
+  end
+
+  def get_exist_keywords
+    @keywords = [Kebab, Turmeric, Szechuan, Cheese, Egg, Lamb, Pasta]
+  end
+
+  def get_user_keyword
+    @keywords.each.with_index do |keyword, index|
+      puts "#{index}. #{keyword}"
+    end
   end
 end
