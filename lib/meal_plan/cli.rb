fddaf2f7dfbb4a99399ab4a2767f1db294c635e4
YUKIKO IKEDA <applelele@gmail.com>
2020-04-24 04:56:57 +0000

    added some definitions to cli.rb

--- lib/meal_plan/cli.rb
+++ lib/meal_plan/cli.rb
@@ -1,21 +1,31 @@
 class MealPlan::CLI
+  attr_accessor :keywords
 
   def call
     puts "Welcome to Meal Plan"
     # binding.pry
     # get_exist_keywords
+    # get_list_keywords
     # get_user_keyword
     # get_recipes
     # list_recipes
+    get_exist_keywords
+    get_list_keywords
   end
 
   def get_exist_keywords
-    @keywords = [Kebab, Turmeric, Szechuan, Cheese, Egg, Lamb, Pasta]
+    @keywords = ["Kebab", "Turmeric", "Szechuan", "Cheese", "Egg", "Lamb", "Pasta"]
   end
 
-  def get_user_keyword
-    @keywords.each.with_index do |keyword, index|
+  def get_list_keywords
+    puts "Choose a keyword to see a recipe"
+    @keywords.each.with_index(1) do |keyword, index|
       puts "#{index}. #{keyword}"
     end
   end
+
+  def get_user_keyword
+    keyword = gets.strip
+  end
+
 end
